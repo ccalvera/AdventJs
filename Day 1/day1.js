733a65ad935efaad4db76dfed7f46c3adc7c4757
d.calvera@utp.edu.co <d.calvera@utp.edu.co>
2022-10-21 11:45:03 -0500

    feat: day1 ready

---
+++ Day 1/day1.js
@@ -0,0 +1,23 @@
+// Considera una lista/array de ovejas.
+// Cada oveja tiene un nombre y un color.
+// Haz una función que devuelva una lista con todas las ovejas que sean de color rojo
+// y que además su nombre contenga tanto las letras n Y a,
+// sin importar el orden, las mayúsculas o espacios.
+
+const ovejas = [
+  { name: "Noa", color: "azul" },
+  { name: "Euge", color: "rojo" },
+  { name: "Navidad", color: "rojo" },
+  { name: "Ki Na Ma", color: "rojo" },
+  { name: "AAAAAaaaaa", color: "rojo" },
+  { name: "Nnnnnnnn", color: "rojo" },
+];
+
+export default function contarOvejas(ovejas) {
+  return ovejas.filter(
+    (camilo) =>
+      camilo.color === "rojo" &&
+      camilo.name.toLowerCase().includes("n") &&
+      camilo.name.toLowerCase().includes("a")
+  );
+}
